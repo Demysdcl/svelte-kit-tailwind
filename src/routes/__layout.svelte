7e8e13db72ae8f09f6f99952d674f894be121828
Demysdcl <demysdcl@gmail.com>
2021-11-22 16:36:33 -0300

    strut: init project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import '../app.css';
+</script>
+<slot>
+
+</slot>
+
+<style>
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-4xl text-indigo p-10">Welcome to SvelteKit</h1>
+<p class="text-gray-500 px-10" >Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
